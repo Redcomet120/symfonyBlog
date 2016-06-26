0d667be44b0dc282396042e2bcc245e755d90ee2
MBlancher <redcomet120@gmail.com>
2016-06-26 15:08:06 -0700

    starting blog controller with endpoints

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+
+ // src/AppBundle/Controller/BlogController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends Controller
+{
+    /**
+     *  @Route("/Blog/home")
+     */
+    // page all posts
+    public function homeAction()
+    {
+        $posts = "Some Blog Post";
+
+        return new Response(
+            '<html><body>Post: '.$posts.'</body></html>'
+        );
+
+    }
+
+    /**
+     *  @Route("/api/Blog/home")
+     */
+    //api call to all posts
+    public function apiHomeAction()
+    {
+        $posts = "Some API post";
+
+        return new JsonResponse($posts);
+    }
+}
+
