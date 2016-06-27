450c99b7cea7ed91324a325f51e5ba2097dd256d
MBlancher <redcomet120@gmail.com>
2016-06-27 00:01:26 -0700

    web server breaks while trying to call function in same controller, need to restructure code or figure out helper methods

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -7,7 +7,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+/*
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+ */
+use AppBundle\Entity\Post;
 //use Symfony\Component\HttpFoundation\Request;
+
+
 
 class BlogController extends Controller
 {
@@ -15,13 +23,22 @@ class BlogController extends Controller
      *  @Route("/Blog/home")
      */
     // page all posts
+    //    public function homeAction(Request $request)
     public function homeAction()
     {
-        $posts = "Some Blog Post";
+        $posts = showAction();
+
+   //     $form = formAction();
+  //      $form->handleRequest($request);
+  //      if ($form->isSubmitted()){
+  //          return $this-redirecttoRoute('/Blog/home');
+  //      }
 
         $html = $this->container->get('templating')->render(
             'blog/home.html.twig',
-            array('posts' => $posts)
+            array('posts' => $posts
+//                'form' => $form->createView()
+            )
         );
         return new Response(
             //  '<html><body>Post: '.$posts.'</body></html>'
@@ -36,9 +53,43 @@ class BlogController extends Controller
     //api call to all posts
     public function apiHomeAction()
     {
-        $posts = "Some API post";
-
+//        $posts = "Some API post";
+        $posts = this->showAction();
         return new JsonResponse($posts);
+    }
+/*
+    public function formAction()
+    {
+
+        $form = $this->createFormBuilder($post)
+            ->add('author', TextType::class)
+            ->add('content', TextType::class)
+            ->add('submit', SubmitType::class, array('label' => 'Submit'))
+            ->getForm();
+        return($form);
+    }
+/*
+    public function createAction()
+    {
+        $post = new Post();
+        $post->setAuthor('Ted Mosby');
+        $post->setContent('Well kids let me tell you the story of how I met your mother');
+
+        $dbadd = $this->getDoctrine()->getManager();
+        $dbadd->persist($post);
+        $dbadd->flush();
+
+        return new Response('Saved new Post:'.$post->getId());
+    }
+ */
+    public function showAction()
+    {
+
+        $repo = $this->getDoctrine()
+            ->getRepository('AppBundle:Post');
+        $posts = $repo->findAll();
+
+        return($posts);
     }
 }
 
---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,89 @@
+<?php
+
+//src/AppBundle/Entity/Post.php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="post")
+ */
+
+class Post
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=75)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Post
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
